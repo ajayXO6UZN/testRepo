d0aca39462094bacb64a1879dd821f339ea31936
ajaySahu <ajay@sahu.edu.in>
2021-10-20 21:14:34 +0530

    backend message commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -29,23 +29,7 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
   
     // checking if user has given password and email both
   
-    if (!email || !password) {
-      return next(new ErrorHandler("Please Enter Email & Password", 400));
-    }
-  
-    const user = await User.findOne({ email }).select("+password");
-  
-    if (!user) {
-      return next(new ErrorHandler("Invalid email or password", 401));
-    }
-  
-    const isPasswordMatched = await user.comparePassword(password);
-  
-    if (!isPasswordMatched) {
-      return next(new ErrorHandler("Invalid email or password", 401));
-    }
-  
-    sendToken(user, 200, res);
+   
   });
 
  // Logout User
