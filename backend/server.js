d255867ff36b3c90d91122a14542716c099578ca
ajaySahu <ajay@sahu.edu.in>
2021-10-21 11:31:06 +0530

    server file change

--- backend/server.js
+++ backend/server.js
@@ -13,15 +13,4 @@ process.on("uncaughtException",(err)=>{
     console.log(`shuting down the server due to uncaught exception`);
 })
 
-const server = app.listen(process.env.PORT,()=>{
-    console.log(`server is running on http://localhost:${process.env.PORT}`);
-})
-
-process.on("unhandledRejection",(err)=>{
-    console.log(`Error: ${err.message}`);
-    console.log(`server is shuting down due to unhandled promise rejection`);
-    server.close(()=>{
-        process.exit(1);
-    })
-})
 
